f87aee62954ebaa4ecb92d42a3727920f7913ac1
AtaKolev <atanaskolevv01@gmail.com>
2022-02-08 10:15:53 +0200

    Attempts to make a working site

---
+++ app.py
@@ -0,0 +1,32 @@
+import pickle
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+try:
+    model = pickle.load(open('model_GradientBoostingClassifier.pkl', 'rb'))
+except:
+    print("Model didn't load!")
+
+@app.route('/')
+def predict():
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    output = model.predict(final_features)
+    
+    if output == 1:
+        return render_template('index.html', prediction_text = 'Client will get a stroke')
+    else:
+        return render_template('index.html', prediction_text = 'Client will not get a stroke')
+
+@app.route('/predict_api', methods = ['POST'])
+def predict_api():
+    data = request.get_json(force = True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
